54be0a9cb8deedc07636b27baffd5bff2898d6fa
wdp150360 <seven@sevendeiMac.local>
2020-06-03 10:26:33 +0800

    提交

--- bankcomm/__init__.py
+++ bankcomm/__init__.py
@@ -1,4 +1,7 @@
 from .config import hostname, port
+import redis
 
+pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+rs = redis.Redis(connection_pool=pool)
 
 __all__ = ['hostname', 'port']
